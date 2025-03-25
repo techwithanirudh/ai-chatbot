366e8acb345470512831fbf1d17ebb52995fa133
Anirudh Sriram <hello@techwithanirudh.com>
2025-03-25 11:28:37 +0000

    fix: 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,19 +7,19 @@ export default function Component() {
     <div className="flex h-full min-h-svh flex-col items-center justify-center">
       <div className="flex max-w-md flex-1 flex-col items-center justify-center gap-4">
         <div className="flex items-center gap-2">
-          <div className="rounded-lg p-2 text-primary-foreground bg-zinc-900">
-            <Bot className="size-6" />
+          <div className="rounded-lg p-2 bg-muted">
+            <Bot className="size-6 text-muted-foreground" />
           </div>
         </div>
         <h1 className="text-4xl font-bold">Page Not Found</h1>
-        <p className="text-lg text-gray-500">
+        <p className="text-lg text-center text-muted-foreground">
           The page you&apos;re looking for doesn&apos;t exist or has been moved.
         </p>
 
         <Button asChild className="w-full">
           <Link href="/">Back To Home</Link>
         </Button>
-        <div className="fixed bottom-4 inset-x-0 flex w-full items-center justify-center gap-2 text-sm text-muted-foreground">
+        <div className="fixed bottom-4 inset-x-0 flex w-full items-center justify-center gap-2">
           <p className="text-sm text-muted-foreground">Error 404</p>
         </div>
       </div>
