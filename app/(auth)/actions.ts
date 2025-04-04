cd71ab8a9327aa21f7c9ae1b476c405f9dd906ba
Anirudh Sriram <hello@techwithanirudh.com>
2025-04-04 12:36:32 +0000

    refactor: clean up signInWithKeycloak function and improve code formatting

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -2,9 +2,7 @@
 import 'server-only';
 
 import { actionClient, ActionError } from '@/lib/safe-action';
-import {
-  MagicLinkSchema
-} from '@/lib/validators';
+import { MagicLinkSchema } from '@/lib/validators';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/server/auth';
 
@@ -20,15 +18,14 @@ export const signInWithMagicLink = actionClient
     // throw new ActionError("Failed to sign in with magic link.");
   });
 
-  export const signInWithKeycloak = async () => {
-    const res = (await signIn('keycloack', {
-      redirect: false,
-    })) as string;
-  
-    if (res) redirect(res);
-    throw new ActionError('Failed to sign in with keycloak.');
-  };
-  
+export const signInWithKeycloak = async () => {
+  const res = (await signIn('keycloack', {
+    redirect: false,
+  })) as string;
+
+  if (res) redirect(res);
+  throw new ActionError('Failed to sign in with keycloak.');
+};
 
 export const signInWithGoogle = async () => {
   const res = (await signIn('google', {
